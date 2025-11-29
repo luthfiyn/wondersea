68d43d1fa9bb54cc5885af01b133ccd268c86a53
luthfiyani <salsabilaluthfiyanii@gmail.com>
2025-11-29 13:42:13 +0700

    bug

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.png'],
+      //includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'maskable-icon.png'],
       manifest: {
         name: 'Wondersea Indonesia',
         short_name: 'Wondersea',
